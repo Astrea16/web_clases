4af240b3bcf0a256c4077a54007e78f9253a3cf5
Astrea <7yatan@gmail.com>
2016-06-20 19:44:49 +0300

    Внесены изменения в js

--- focal1/js/js.js
+++ focal1/js/js.js
@@ -1,8 +1,33 @@
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-  infiniteLoop: false,
-  hideControlOnEnd: true
+$(document).ready(function() {
+	$('.bxslider').bxSlider({
+		infiniteLoop: false,
+		hideControlOnEnd: true
+	});
+	
+	$("#form").off('submit').on('submit', function() {
+		var form = $(this),
+			submit = form.find('[type="submit"]');
+		
+		submit.attr('disabled', true);
+		
+		$.ajax({
+			type: "POST",
+			url: "post.php",
+			data: form.serialize(),
+			success: function(result) {
+				$(document).scrollTop(form.offset().top ); 
+				
+				if (result) {
+					$('#result').html(result).parent().removeClass('hidden');
+					form.find("input[type=text], input[type=email], textarea").val("");
+				} else {
+					$('#result').parent().addClass('hidden');
+				}
+				
+				submit.attr('disabled', false);
+			}
 		});
- 		 	
+		
+		return false;
+	});
 });
-   
